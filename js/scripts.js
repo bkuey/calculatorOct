b9970599ed9900b219ea32139c38fbee789f1547
Anton Sharykin and Mai Kue Lor <anton@>
2019-10-21 13:47:56 -0700

    celsius and fahrenheit exercise

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,18 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = divide(number1, number2);
 alert(result);
+
+function convert1(input1) {
+	alert(parseFloat((input1 - 32) * 5/9));
+};
+
+var f = prompt("fahrenheit");
+var result = convert1(f);
+
+
+function convert(input) {
+	alert(parseFloat(input * 9/5 + 32));
+};
+
+var c = prompt("celsius:");
+var result = convert(c);
